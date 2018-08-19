9e5a43616deff95379160fe5320dc212023c01f7
cart3ch <karthigeyan.r@hotmail.com>
2018-08-19 19:59:07 +0530

    testing with a new file

--- file1.R
+++ file1.R
@@ -1,4 +1,2 @@
 mtcars
-git config --global user.email "karthigeyan.r@hotmail.com"
-git config --global user.name "cart3ch"
-women
+plot(women)
---
+++ file2.R
@@ -0,0 +1,6 @@
+
+x=1:100
+y<-1:100
+x
+install.packages('car')
+y<-seq(1,100,by=2)
